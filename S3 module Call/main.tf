19b7417b8bbfe1165fefba39d72f6f6fa907c70f
Ikomo007 <livingrock.iyke@gmail.com>
2023-09-20 12:03:30 -0400

    fix: modified. Contributing-to: https://team32g02.atlassian.net/jira/software/projects/BC32/boards Problem Solution Note

--- S3 module Call/main.tf
+++
@@ -1,3 +0,0 @@
-#module "s3" {
-#  source = "https://github.com/Ikomo007/s3Backend.git//s3_Module"
-#}
---
+++ S3moduleCall/main.tf
@@ -0,0 +1,3 @@
+module "s3" {
+  source = "git@github.com:Ikomo007/s3Backend.git//s3_Module"
+}
