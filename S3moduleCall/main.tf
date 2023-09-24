e4a9ba9569b042908dfc4de57a96dcc239c3580f
Ikomo007 <livingrock.iyke@gmail.com>
2023-09-24 01:38:08 -0400

    srelease

--- S3moduleCall/main.tf
+++ S3moduleCall/main.tf
@@ -1,3 +1,3 @@
 module "S3-module" {
-  source = "git::https://github.com/Ikomo007/s3Backend.git//s3-Module?ref=v1.0.0?=v1.0.0"
+  source = "git::https://github.com/Ikomo007/s3Backend.git//s3-Module"
 }
